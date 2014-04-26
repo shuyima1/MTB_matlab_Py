c7c46de078f12b166de30d15dae9f74c82114cb7
shuyima1 <dreamcatcher284@gmail.com>
2014-04-25 18:47:01 -0700

    first complete draft of python implementation of quantile normalization

--- quantile.py
+++ quantile.py
@@ -8,6 +8,18 @@ def quantilenorm(matrix):
 	for col in range(matrix.shape[1]):
 		rankedmat[:,col] = ranking(matrix[:,col])
 
+	sortedmat = np.empty(shape=matrix.shape)
+	for col in range(matrix.shape[1]):
+		sortedmat[:,col] = matrix[matrix[:,col].argsort(),col]
+
+	rowavg = sortedmat.mean(axis=1)
+	qnmat = np.empty(shape=matrix.shape,dtype=int)
+	for count,avg in enumerate(rowavg):
+		ix = np.where(rankedmat == count)
+		ix = tuple(zip(*ix)))
+		for coord in ix:
+			qnmat[coord[0],coord[1]] = rowavg[count]
+
 	return qnmat
 
 def ranking(vector):
